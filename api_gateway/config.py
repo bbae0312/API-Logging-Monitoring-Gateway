b12b2b7d7ef39246f6ce6abc2d6fc8d5fcb6c0ac
jefgu <jgu6661@gmail.com>
2024-11-30 19:28:34 -0800

    added requirements

--- api_gateway/config.py
+++ api_gateway/config.py
@@ -1,3 +1,3 @@
 USER_SERVICE_URL = "http://localhost:5001"
-DOCUMENT_SERVICE_URL = "http://localhost:5003"  # New constant for document service URL
+DOCUMENT_SERVICE_URL = "http://localhost:5003"
 LOG_FILE_PATH = "logs/api_gateway.log"
